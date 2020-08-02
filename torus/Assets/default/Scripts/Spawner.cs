192ffe59a0251a1213ea08e6e98754e52e4b395d
Daniel Hogg <dan300vgames@gmail.com>
2020-08-02 22:51:28 +0100

    initial dots

---
+++ torus/Assets/default/Scripts/Spawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+
+
+
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField] private Mesh unitMesh;
+    [SerializeField] private Material unitMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeEntity();
+    }
+
+    private void MakeEntity()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype archatype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            );
+
+        Entity myEntity = entityManager.CreateEntity(archatype);
+
+        entityManager.AddComponentData(myEntity, new Translation
+        {
+            Value = new float3(2f, 0f, 4f)
+        });
+
+        entityManager.AddSharedComponentData(myEntity, new RenderMesh
+        {
+            mesh = unitMesh,
+            material  = unitMaterial
+        });
+
+        entityManager.CreateEntity();
+
+
+
+
+
+    }
+
+
+}
---
+++ torus/Assets/default/Scripts/cameralook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class cameralook : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public Rigidbody RB;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Convert.ToSingle(0.1);
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
