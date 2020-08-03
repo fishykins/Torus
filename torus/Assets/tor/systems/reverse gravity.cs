5b4716e61355950455e50b31e9f7e0a939169980
Daniel Hogg <dan300vgames@gmail.com>
2020-08-03 23:20:45 +0100

    centrapedal force and its calculation

---
+++ torus/Assets/tor/systems/reverse gravity.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using System;
+
+public class ReverseGravity : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation trans, ref Centrapedalforce centrapedalforce) =>
+        {
+            float sin = math.sin(centrapedalforce.value);
+            float cos = math.cos(centrapedalforce.value);
+        });
+
+    }
+}
